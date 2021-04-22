ef3470ed1ea3f801384080de03727fc6f6bcc006
Laxman Reddy Aileni <59108427+LaxmanReddyAileni@users.noreply.github.com>
2021-04-22 12:47:35 -0700

    Commit done
    Commit done

--- Django-ecommerce-main/run.sh
+++
@@ -1,9 +0,0 @@
-cd ecommerce/
-python manage.py makemigrations
-python manage.py migrate
-# run migrations to add some data
-cd ../
-chmod +x migrations.sh
-./migrations.sh
-cd ecommerce/
-python manage.py runserver  0.0.0.0:8000
